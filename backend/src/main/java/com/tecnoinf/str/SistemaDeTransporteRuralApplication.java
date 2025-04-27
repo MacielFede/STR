7e8c9890c4fe1ad32bd99ca7c82e3d8bbf30e22e
Federico Maciel <federico.maciel@abitab.com.uy>
2025-04-27 11:23:47 -0300

    scaffold the project

---
+++ backend/src/main/java/com/tecnoinf/str/SistemaDeTransporteRuralApplication.java
@@ -0,0 +1,13 @@
+package com.tecnoinf.str;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeTransporteRuralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeTransporteRuralApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tecnoinf/str/SistemaDeTransporteRuralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnoinf.str;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeTransporteRuralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
